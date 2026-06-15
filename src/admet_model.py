d625164b73c5478b3a2ff8018d335c4fe7dfdfae
kath <kath45823@gmail.com>
2026-06-15 16:06:53 -0700

    fix: creates dataset of all records

--- src/admet_model.py
+++ src/admet_model.py
@@ -8,18 +8,25 @@ query_params = {
     'limit': 1000
 }
 
-response = requests.get('https://www.ebi.ac.uk/chembl/api/data/activity', params=query_params)
-data = response.json()
 chembl_data = []
+offset = 0
 
-for a in data["activities"]:
-    smiles = a["canonical_smiles"]
-    pic50 = a["pchembl_value"]
+while True: 
+    query_params['offset'] = offset
+    response = requests.get('https://www.ebi.ac.uk/chembl/api/data/activity', params=query_params)
+    data = response.json()
+    total = data['page_meta']['total_count']
 
-    if smiles is None or pic50 is None: 
-        continue
+    for a in data["activities"]:
+        smiles = a["canonical_smiles"]
+        pic50 = a["pchembl_value"]
 
-    chembl_data.append({'smiles': smiles, 'pIC50': pic50})
+        if smiles is None or pic50 is None: 
+            continue
 
-print(len(chembl_data))
+        chembl_data.append({'smiles': smiles, 'pIC50': pic50})
+    
+    offset += 1000
+    if offset >= total: 
+        break
 
