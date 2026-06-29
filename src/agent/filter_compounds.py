e5c3ab6a4aa830bdbf94cdb1e2543e6adb7ab9ec
kath <kath45823@gmail.com>
2026-06-29 13:59:42 -0700

    feat: get top 100 compounds with the highest pic50 score

--- src/agent/filter_compounds.py
+++ src/agent/filter_compounds.py
@@ -1,5 +1,4 @@
 import requests
-import pandas as pd
 from src.model.chemprop_model import predict_pic50 
 
 def get_data():
@@ -47,6 +46,10 @@ def filter_compounds():
     for smiles in data: 
         score = predict_pic50(smiles)
         if score >= 6: 
-            filtered_compounds.append(smiles)
-    
-    return filtered_compounds 
+            filtered_compounds.append((smiles, score))
+
+    sorted_compounds = sorted(filtered_compounds, key = lambda elt: elt[1], reverse = True)
+    sorted_compounds = sorted_compounds[:100]
+    top_compounds = [smi for smi, sc in sorted_compounds]
+
+    return top_compounds
